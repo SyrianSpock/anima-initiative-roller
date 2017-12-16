4807107a36c16ef73cf240803cd73075aef47fea
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2017-12-16 18:26:45 +0100

    Handle failure modifier different from default (3)

--- roll.py
+++ roll.py
@@ -1,11 +1,11 @@
 import argparse
-from collections import namedtuple
+from collections import namedtuple, Iterable
 import logging
 import operator
 import random
 import yaml
 
-Player = namedtuple('Player', ['name', 'initiative'])
+Player = namedtuple('Player', ['name', 'initiative', 'fail'])
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description=__doc__)
@@ -16,17 +16,25 @@ def parse_arguments():
 
 def read_modifiers(yaml_file):
     with open(yaml_file) as file:
-        return yaml.load(file)
+        modifiers = yaml.load(file)
 
-def roll_initiative(name, modifier):
+        failures = {}
+        for player in modifiers:
+            if isinstance(modifiers[player], Iterable):
+                failures = {player: elem['fail'] for elem in modifiers[player][1:]}
+                modifiers[player] = modifiers[player][0]
+
+        return modifiers, failures
+
+def roll_initiative(name, modifier, fail):
     initiative = modifier + roll_open()
-    return Player(name=name, initiative=initiative)
+    return Player(name=name, initiative=initiative, fail=fail)
 
 def roll_open(base_roll=0, success=90, fail=3):
     roll = roll_100()
     logging.debug("Rolling {}, rolled {}, with success {}".format(base_roll, roll, success))
     if roll >= success:
-        return roll_open(base_roll= roll + base_roll, success=success+1)
+        return roll_open(base_roll= roll + base_roll, success=success+1, fail=fail)
     else:
         res = roll + base_roll
         if base_roll == 0 and roll <= fail:
@@ -36,15 +44,18 @@ def roll_open(base_roll=0, success=90, fail=3):
 def roll_100():
     return random.randint(1,100)
 
+def show_initiatives(players):
+    for player in players:
+        print('{}:\t{}'.format(player.name, player.initiative))
+
 def main():
     args = parse_arguments()
 
-    modifiers = read_modifiers(args.file)
-    players = [roll_initiative(player, modifiers[player]) for player in modifiers]
-    players = sorted(players, key=lambda player: player.initiative, reverse=True)
+    modifiers, failures = read_modifiers(args.file)
+    players = [roll_initiative(player, modifiers[player], failures.get(player, 3)) for player in modifiers]
 
-    for player in players:
-        print('{}:\t{}'.format(player.name, player.initiative))
+    players = sorted(players, key=lambda player: player.initiative, reverse=True)
+    show_initiatives(players)
 
 if __name__ == '__main__':
     main()
